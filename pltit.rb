c37432ea219d6a2f1c5c56430bbedd89e202ce76
Uwe Gotzes <u.gotzes@gmail.com>
2021-06-08 12:24:07 +0200

    added script to quickly plot multiple variables

---
+++ pltit.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+puts "usage: ./pltit.rb test_case variable_list_from_solution_file"
+puts "example: ./pltit.rb da2 \"var_pipe_Qo_in\\[EN_aux0,EN\" \"entry_nom_TA\\[EN_aux0,EN\" \"var_pipe_Qo_in\\[EH_aux0,EH\" \"entry_nom_TA\\[EH_aux0,EH\"\n\n"
+require 'csv'
+
+def hash2csv(h,file)
+  csv = h.keys.join(';') + "\n"
+  csv += h.values.transpose.map{ |x| x.join(';') }.join("\n")
+  File.write(file, csv)
+end
+
+data = {}
+Dir.glob('instances/' + ARGV[0] + '/output/*.sol') do |sol_filename|
+  File.open(sol_filename).each do |line|
+    x = ARGV[1..-1].map{ |a| line.match(a + '.*') }.compact
+    if x[0] != nil
+      tup = x[0][0].split(" ")
+      data[tup[0]] ||= []
+      data[tup[0]] << tup[1].to_f
+    end
+  end
+end
+hash2csv(data, 'pltit.csv')
+
+plt = "set key autotitle columnhead noenhanced\nset datafile separator ';'\n"
+plt += "plot for [i=1:10] 'pltit.csv' u i w l"
+File.write('pltit.plt', plt)
+system('gnuplot -p pltit.plt')
