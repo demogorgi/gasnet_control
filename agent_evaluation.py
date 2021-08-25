8ad133a48fee00a3209d4553ee11930b09307912
Adrian Goess <adrian.goess@fau.de>
2021-08-25 11:20:53 +0200

    evaluated an action epsilon of 10

--- agent_evaluation.py
+++ agent_evaluation.py
@@ -11,6 +11,8 @@ num_eval_agent_steps = 80
 
 simulations_per_agent_step = 1
 
+action_epsilon = 10
+
 # evaluation procedure
 # import the policy accomplished through training
 temp_dir = os.getcwd() + '/instances'
@@ -20,13 +22,13 @@ policy_dir = os.path.join(temp_dir, "policy_" +\
                           f"rate1e-2to1e-05_" +\
                           f"clip{None}_" +\
                           f"update{100}_" +\
-                          f"epsilondecay10to0001_run5")
+                          f"epsilondecay10to0001_run10")
                           #f"boltzmann{0.1}")
 trained_policy = tf.compat.v2.saved_model.load(policy_dir)
 
 # define the environment with nominations from file
 eval_py_env = network_environment.GasNetworkEnv(
-    action_epsilon=5,
+    action_epsilon=action_epsilon,
     convert_action=True,
     steps_per_agent_step=simulations_per_agent_step,
     max_agent_steps=num_eval_agent_steps,
@@ -40,7 +42,7 @@ eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
 time_step = eval_env.reset()
 step = 0
 for _ in range(num_eval_agent_steps):
-    time.sleep(5)
+    #time.sleep(5)
     print("#"*15 + f"Evaluation of step {step}" + "#"*15)
     if not time_step.is_last():
         action_step = trained_policy.action(time_step)
