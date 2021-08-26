6c79a38384d5a77a5a2b2d09831725748469b65f
Adrian Goess <adrian.goess@fau.de>
2021-08-26 06:59:05 +0200

    enhanced the yml test read script out of interest

--- test_read_yml.py
+++ test_read_yml.py
@@ -2,20 +2,29 @@ import yaml
 import os
 import time
 
+nreps = 10000
+
 start = time.time()
 
-with open(os.path.join('./instances/da2', 'init_decisions_backupAllCombi.yml')) as file:
-    agent_decisions = yaml.load(file, Loader=yaml.FullLoader)
+for _ in range(nreps):
+    with open(os.path.join('./instances/da2', 'init_decisions_backupAllCombi.yml')) as file:
+        agent_decisions = yaml.load(file, Loader=yaml.FullLoader)
 
 end = time.time()
 
-print("Time elapsed: {}".format(end - start))
+time_elapsed_long = end - start
+print(f"Time elapsed: {time_elapsed_long}")
+print("Time elapsed per loading: {}".format(time_elapsed_long/nreps))
 
-start = end
+start = time.time()
 
-with open(os.path.join('./instances/da2', 'init_decisions.yml')) as file:
-    agent_decisions = yaml.load(file, Loader=yaml.FullLoader)
+for _ in range(nreps):
+    with open(os.path.join('./instances/da2', 'init_decisions.yml')) as file:
+        agent_decisions = yaml.load(file, Loader=yaml.FullLoader)
 
 end = time.time()
 
-print("Time elapsed: {}".format(end - start))
+time_elapsed = end - start
+print(f"Time elapsed: {time_elapsed}")
+print("Time elapsed per loading: {}".format(time_elapsed/nreps))
+print(f"Factor of first to second run {time_elapsed_long/time_elapsed}")
