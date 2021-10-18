edba342d888901d437125284087571b6b6bc3906
Adrian Goess <adrian.goess@fau.de>
2021-10-18 18:32:29 +0200

    enhanced the iterations to 400k

--- cdqn_agent_training.py
+++ cdqn_agent_training.py
@@ -97,7 +97,7 @@ else:
     no_run = -1
 
 
-in_num_iterations_options = [200_000]#[200000]#[5000, 20000, 50000]
+in_num_iterations_options = [400_000]#[200000]#[5000, 20000, 50000]
 in_boltzmann_temperatures = []
 # in_target_update_steps_options = [5000] #100, 250, 400, 550, 700, 850, 1000
 
